da28ce7f09bef521962d8df17c4a83cce063a9ff
mstefanov <mstefanov@rnd-solutions.net>
2021-06-27 12:55:47 +0300

    wip: showing polygons....

--- express_backend/server-config.js
+++ express_backend/server-config.js
@@ -1,10 +1,10 @@
 var config = {};
 
-config.appId = "83yis69cba"; 
-config.hashToken = "ODN5aXM2OWNiYXxPTjUzdUpBRURqM1l3a3JNSTRRRjYxd1lMYmtKRU1mRmEzd3EyNkZn";
+config.appId = "07qut09bm6";
+config.hashToken =
+  "MDdxdXQwOWJtNnxpZXZwdEtmRWd5MTV2dFlocGQ0ZHc0RXZybjdwSGJ6VDVoa2p6cWJE";
 config.authTokenUrl = "https://api.iq.dev.inrix.systems/auth/v1/appToken";
 
 module.exports = config;
-
 
 /* "start": "start http://localhost:3000 & npm run dev " */
