221efe274b244ab48d68aa848ec9ed52c7a1483e
mstefanov <mstefanov@rnd-solutions.net>
2021-07-12 13:55:30 +0300

    refactor: updating lots example to use real endpoint

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.appId = "07qut09bm6"; 
-config.hashToken = "MDdxdXQwOWJtNnxpZXZwdEtmRWd5MTV2dFlocGQ0ZHc0RXZybjdwSGJ6VDVoa2p6cWJE";
-config.authTokenUrl = "https://api.iq.dev.inrix.systems/auth/v1/appToken";
-
+config.appId = "0vl5hsc1m5";
+config.hashToken =
+  "MHZsNWhzYzFtNXx6dWJSUFpucU9OYXF6ZmlIaXdtcWI0Wm8zNjB1eVUwOTczcmthcm1Y";
+config.authTokenUrl = "https://api.iq.inrix.systems/auth/v1/appToken";
 module.exports = config;
