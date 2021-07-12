47b831ab4d4a9edbf0efd3b867d61032c667930d
Nevyan Neykov <nevyan.neykov@rnd-solutions.net>
2021-07-12 17:18:25 +0300

    feat: incidents ui

--- ui/client-config.js
+++ ui/client-config.js
@@ -8,6 +8,6 @@ config = {
     "pk.eyJ1IjoibmV2eWFuIiwiYSI6ImNrcWpoMTZnNDAxdWYyd3IwcDI5OWE4MWgifQ.OiNfLuto4nueNcIifTe5-g",
   mapboxUrl:
     "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=",
-  lotsAPI: "https://api.iq.inrix.systems/parking/v3/lots"
-
+  lotsAPI: "https://api.iq.inrix.systems/parking/v3/lots",
+  incidentsAPI: "https://incident-api.inrix.com/v1/incidents",
 };
