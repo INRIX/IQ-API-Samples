15ae2451bdbc610a8c6a74e5a28076115d65e450
mstefanov <mstefanov@rnd-solutions.net>
2021-08-12 17:35:48 +0300

    refactor: new endpoints for the APIs

--- api/config.js
+++ api/config.js
@@ -3,5 +3,5 @@ var config = {};
 config.appId = "0vl5hsc1m5";
 config.hashToken =
   "MHZsNWhzYzFtNXx6dWJSUFpucU9OYXF6ZmlIaXdtcWI0Wm8zNjB1eVUwOTczcmthcm1Y";
-config.authTokenUrl = "https://api.iq.inrix.systems/auth/v1/appToken";
+config.authTokenUrl = "https://api.iq.inrix.com/auth/v1/appToken";
 module.exports = config;
--- ui/client-config.js
+++ ui/client-config.js
@@ -1,15 +1,14 @@
 config = {
   checkConfigAPI: "http://localhost:3000/checkConfig",
   inrixTokenAPI: "http://localhost:3000/getToken",
-  inrixIncidentsAPI: "https://api.iq.inrix.systems/traffic/v1/incidents",
-  inrixTilesAPI: "https://api.iq.inrix.systems/traffic/v1/tiles/",
-  inrixLotsAPI: "https://api.iq.inrix.systems/parking/v3/lots",
-  inrixBlocksAPI: "https://api.iq.inrix.systems/parking/v3/blocks",
-  inrixDriveTimePolygonsAPI:
-    "https://api.iq.inrix.systems/traffic/drivetimePolygons",
-  inrixTravelTimesFindAPI: "https://api.iq.inrix.systems/traffic/routing/find",
-  inrixCamerasAPI: "https://api.iq.inrix.systems/traffic/trafficCameras/camerasInRadius",
-  inrixCamerasImageAPI: "https://api.iq.inrix.systems/traffic/trafficCameras/cameraImage",
+  inrixIncidentsAPI: "https://api.iq.inrix.com/v1/incidents",
+  inrixTilesAPI: "https://api.iq.inrix.com/traffic/v1/tiles/",
+  inrixLotsAPI: "https://api.iq.inrix.com/parking/lots/v3",
+  inrixBlocksAPI: "https://api.iq.inrix.com/parking/blocks/v3",
+  inrixDriveTimePolygonsAPI: "https://api.iq.inrix.com/drivetimePolygons",
+  inrixTravelTimesFindAPI: "https://api.iq.inrix.com/findRoute",
+  inrixCamerasAPI: "https://api.iq.inrix.com/trafficCamerasInRadius",
+  inrixCamerasImageAPI: "https://api.iq.inrix.com/trafficCameraImage",
   openStreetMapAPI: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   mapboxToken:
     "pk.eyJ1IjoibmV2eWFuIiwiYSI6ImNrcWpoMTZnNDAxdWYyd3IwcDI5OWE4MWgifQ.OiNfLuto4nueNcIifTe5-g",
